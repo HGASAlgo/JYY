5bc5ae2fdfa698657303e2bfa294ac90865cf5ca
JangYoonYoung <jyy0325@naver.com>
2020-03-20 18:07:04 +0900

    200320

---
+++ 백준_알고리즘/10809.java
@@ -0,0 +1,20 @@
+/*소문자 a부터 z까지 입력받은 문자열 내 첫번째 위치 반환 
+*사용되지 않은 소문자 위치는 -1 반환.
+*문자열.indexOf("찾을 문자열") : 일치하는 문자열의 첫번째 위치 반환
+*/
+
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String word = br.readLine();
+		
+		for(int i='a'; i<='z'; i++)
+            sb.append(word.indexOf(i)).append(" "); 
+		System.out.println(sb);
+        br.close();
+	}
+}
+
